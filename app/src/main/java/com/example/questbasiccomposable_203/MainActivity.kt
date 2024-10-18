e7c42eb42136628d42e6ae38e1cd85f23bba3f18
Anggia Intan <intananggia23@gmail.com>
2024-10-18 22:43:23 +0700

    menampilkan gambar yang diambil dari resource (R.drawable.umy) ke dalam layout

--- app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,6 +49,10 @@ fun BasicComposable(modifier: Modifier = Modifier) {
             )
         )
         Text(text = "Ini adalah halaman login")
+        Image(painter = painterResource(
+            id = R.drawable.umy
+        ),
+            contentDescription = null,)
     }
 
 
