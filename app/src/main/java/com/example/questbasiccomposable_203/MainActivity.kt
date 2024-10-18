40935300cf2a12c99f451bbd56451071334997e1
Anggia Intan <intananggia23@gmail.com>
2024-10-18 21:07:08 +0700

    mengatur semua elemen di dalam Column ditampilkan di tengah-tengah layar secara horizontal dan vertikal

--- app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.questbasiccomposable_203.ui.theme.QuestBasicComposable_203Theme
@@ -30,6 +33,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BasicComposable(modifier: Modifier = Modifier) {
+    Column (horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        )
 }
 
 @Preview(showBackground = true)
