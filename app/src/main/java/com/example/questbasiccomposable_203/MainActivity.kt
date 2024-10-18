e0ad056a3ee3819f5138d8716191b7891016572d
Anggia Intan <intananggia23@gmail.com>
2024-10-18 14:56:57 +0700

    mengganti fungsi yang dipanggil dalam Scaffold dari Greeting menjadi BasicColumn

--- app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
@@ -20,9 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestBasicComposable_203Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                    BasicColumn(modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
