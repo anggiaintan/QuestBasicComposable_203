0b2ef3ec729d4bbcab851920a9396df89e2e979b
Anggia Intan <intananggia23@gmail.com>
2024-10-18 23:05:08 +0700

    mengatur jarak, ukuran, dan bentuk foto intan

--- app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -70,7 +72,11 @@ fun BasicComposable(modifier: Modifier = Modifier) {
         Image(painter = painterResource(
             id = R.drawable.intan
         ),
-            contentDescription = null,)
+            contentDescription = null,
+            modifier = Modifier
+                .padding(vertical = 20.dp)
+                .size(350.dp)
+                .clip(CircleShape))
     }
 
 
