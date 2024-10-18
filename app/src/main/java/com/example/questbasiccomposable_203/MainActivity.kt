f8f03e96a61099b49399d818cc91aed77d8dc5c0
Anggia Intan <intananggia23@gmail.com>
2024-10-18 15:40:51 +0700

    mengganti fungsi Greeting menjadi BasicComposable

--- app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
+++ app/src/main/java/com/example/questbasiccomposable_203/MainActivity.kt
@@ -29,11 +29,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BasicComposable(modifier: Modifier = Modifier) {
 }
 
 @Preview(showBackground = true)
